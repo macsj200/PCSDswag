01c0c3cdbdf1e216379deffacd9f3a6ce3f88ab8
Max Johansen <macsj200@gmail.com>
2014-08-18 00:21:16 -0600

    idk

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ site nommer
 so i don't have to do it
 '''
 import dailySignupScraper
+import cardScraper
 import libStuff
 import sys
 
@@ -10,4 +11,4 @@ if len(sys.argv) != 3:
 	raise ValueError('You need to pass username and password as arguments')
 
 exportFile = open('paintMixerExports.json', 'w')
-libStuff.writeListToFile(exportFile, dailySignupScraper.scrape(libStuff.login(sys.argv[1], sys.argv[2])))
+libStuff.writeListToFile(exportFile, cardScraper.scrape(libStuff.login(sys.argv[1], sys.argv[2])))
